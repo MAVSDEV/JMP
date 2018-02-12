c81f023dca53b97d30c24bcf3c5803370c7751f2
Uladzimir Malko <uladzimir_malko@epam.com>
2018-02-12 14:04:39 +0300

    created multi-module maven project

---
+++ multi-maven/web/src/test/java/com/epam/mutlimaven/BasicTest.java
@@ -0,0 +1,16 @@
+package com.epam.mutlimaven;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Basic Test.
+ */
+public class BasicTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue("Multi maven module with 'TRUE' condition.", Boolean.TRUE);
+    }
+
+}
